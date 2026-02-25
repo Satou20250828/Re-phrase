03d7a7f73ef26e372caad93dcb2fc40055f1bdaf
さとう <nimu3103@gmail.com>
2026-02-25 22:40:16 +0000

    RuboCopのMethodLength違反を解消

--- app/controllers/rephrases_controller.rb
+++ app/controllers/rephrases_controller.rb
@@ -201,15 +201,17 @@ class RephrasesController < ApplicationController
     @search_logs = SearchLog.order(created_at: :desc).limit(10)
 
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.update(
-          "error_modal_container",
-          partial: "rephrases/error_modal",
-          locals: { error_message: @error_message, field_errors: @field_errors, rephrase: @rephrase }
-        ), status: :ok
-      end
+      format.turbo_stream { render_validation_errors_turbo_stream }
       format.html { render :index, status: :unprocessable_content }
     end
+  end
+
+  def render_validation_errors_turbo_stream
+    render turbo_stream: turbo_stream.update(
+      "error_modal_container",
+      partial: "rephrases/error_modal",
+      locals: { error_message: @error_message, field_errors: @field_errors, rephrase: @rephrase }
+    ), status: :ok
   end
 
   def content_too_long?
